3b502589ee8080bc26328e93c9a689167f175446
James Rutledge <jarutledge2@gmail.com>
2015-07-21 15:21:50 -0500

    Plot create functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :correspondents
+  has_many :plots
 
   
 end
