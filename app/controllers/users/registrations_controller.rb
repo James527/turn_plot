ddcb0a31ac79a29b0eb4e3a5e165c2f1d18fed1a
James Rutledge <jarutledge2@gmail.com>
2015-07-23 03:24:41 -0500

    almost have the plot coordinate generator working

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-before_filter :configure_sign_up_params, only: [:create]
-before_filter :configure_account_update_params, only: [:update]
+  before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_account_update_params, only: [:update]
+  after_action :new_plot_coordinates, only: [:create]
 
   # GET /resource/sign_up
   def new
@@ -58,4 +59,48 @@ before_filter :configure_account_update_params, only: [:update]
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+# def generate_plot_coordinates  
+# end
+
+  def new_plot_coordinates
+    @new_user = current_user
+
+    last_user = User.last
+    lastX = last_user.x
+    lastY = last_user.y
+
+    puts lastX
+    puts lastY
+
+    if (lastX - 1 > lastY)
+      newX = lastX
+      newY = lastY + 1
+      @new_coordinates = [newX, newY]
+    elsif (lastX <= lastY - 1)
+      newY = lastY
+      newX = lastX + 1
+      @new_coordinates = [newX, newY]
+    elsif (lastX - 1 === lastY)
+      newX = 1
+      newY = lastX
+      @new_coordinates = [newX, newY]
+    elsif (lastX === lastY)
+      newX = lastX + 1
+      newY = 1
+      @new_coordinates = [newX, newY]
+    end
+    puts @new_coordinates
+
+    @plotX = @new_coordinates[0]
+    @plotY = @new_coordinates[1]
+
+    # puts @plotX
+    # puts @plotY
+
+    @new_user.x = @plotX
+    @new_user.y = @plotY
+    @new_user.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   # Plot routes
   resources :plots
   # Activate this plot
+  post 'initial' => 'plots/plots#create_initial_plot'
 
   # Coordinate and User Plot routes
   # get 'plot/:x' => 'plots#index'
